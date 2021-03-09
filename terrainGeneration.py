909fe65fb576ec135596cea46e7ff9eb01dfa9fb
Richard Thompson <38708439+TheOtowngman@users.noreply.github.com>
2021-03-08 20:09:58 -0500

    Added Perlin Noise
    Implemented Perlin Noise to help create terrain that feels much more natural than truly random terrain generation.

--- terrainGeneration.py
+++ terrainGeneration.py
@@ -8,8 +8,6 @@ import math
 # Perlin Noise Algorithim based on: https://rtouti.github.io/graphics/perlin-noise-algorithm
 
 '''----- GLOBAL VARAIBLES -----'''
-randomNumberRange = [-1.0,1.5]
-
 amplitude = 1.2
 ordFreq = 1.5
 angFreq = 2 * math.pi * ordFreq
@@ -27,6 +25,8 @@ cmds.text(label='Terrain Generator')
 cmds.intSliderGrp('width', label='Width', min=1, max=25)
 cmds.intSliderGrp('height', label='Length', min=1, max=25)
 cmds.intSliderGrp('subD', label='Subdivision Density', min=1, max=3)
+cmds.floatSliderGrp('minHeight', label='Min Terrain Height', min=-2.0, max=0.0)
+cmds.floatSliderGrp('maxHeight', label='Max Terrain Height', min=1.0, max=2.0)
 
 cmds.button(label='Generate Terrain', command='generateTerrain()')
 
@@ -36,7 +36,7 @@ def vectorFromPoints(pointA, pointB):
     return[pointB[0] - pointA[0], pointB[1] - pointA[1], pointB[2] - pointA[2]]
 
 def getDotProduct(vectorA, vectorB):
-    return ((vectorA[0] * vectorB[0]) + (vectorA[1] * vectorB[1]) + (vectorA[2] * vectorB[2]))
+    return ((vectorA[0] * vectorB[0]) + (vectorA[1] * vectorB[1]))
 
 def setSinHeight(tValue):
     return(amplitude*math.sin((angFreq*tValue)+phase))
@@ -57,9 +57,30 @@ def createPermutation():
     
     return p
 
-def Noise2D(x,z):
-    X = math.floor(x) & 255
-    Z = math.floor(z) & 255
+def lerpValues(t, a1, a2):
+    return (a1 + t*(a2-a1))
+
+def fade(t):
+    return((6*t - 15)*t + 10)*t*t*t
+
+def getConstantVector(v):
+    h = int(v) & 3
+    if(h == 0):
+        return [1.0,1.0]
+    elif(h == 1):
+        return [-1.0, 1.0]
+    elif(h == 2):
+        return [-1.0,-1.0]
+    else:
+        return [1.0,-1.0]
+
+# A function to map a number from one range, to another range. Code adapted from: https://stackoverflow.com/questions/12931115/algorithm-to-map-an-interval-to-a-smaller-interval/12931306
+def mapValue(originLow, originHigh, newLow, newHigh, valueIn):
+    return (valueIn - originLow)*(newHigh-newLow)/(originHigh-originLow) + newHigh 
+
+def Noise2D(x,z,permIn):
+    X = int(math.floor(x)) & 255
+    Z = int(math.floor(z)) & 255
 
     xFloor = x - math.floor(x)
     zFloor = z - math.floor(z)
@@ -69,13 +90,28 @@ def Noise2D(x,z):
     bRight = [xFloor-1.0, zFloor]
     bLeft = [xFloor, zFloor]
 
-    tRightDot = getDotProduct
+    valTRight = permIn[permIn[X+1]+Z+1]
+    valTLeft = permIn[permIn[X]+Z+1]
+    valBRight = permIn[permIn[X+1]+Z]
+    valBLeft = permIn[permIn[X]+Z]
+
+    dotTRight = getDotProduct(tRight, getConstantVector(valTRight))
+    dotTLeft = getDotProduct(tLeft, getConstantVector(valTLeft))
+    dotBRight = getDotProduct(bRight, getConstantVector(valBRight))
+    dotBLeft = getDotProduct(bLeft, getConstantVector(valBLeft))
+
+    u = fade(xFloor)
+    v = fade(zFloor)
+
+    return lerpValues(u, lerpValues(v, dotBLeft, dotTLeft), lerpValues(v, dotBRight, dotTRight))
 
 def generateTerrain():
     # Get the values from the ui
     pWidth = cmds.intSliderGrp('width', query=True, value=True)
     pHeight = cmds.intSliderGrp('height', query=True, value=True)
     pSubDensity = cmds.intSliderGrp('subD', query=True, value=True)
+    pMinHeight = cmds.floatSliderGrp('minHeight', query=True, value=True)
+    pMaxHeight = cmds.floatSliderGrp('maxHeight', query=True, value=True)
 
     # Create a plane based on the user input
     pTerrain = cmds.polyPlane(width=pWidth, height=pHeight, subdivisionsX=pWidth*pSubDensity, subdivisionsY=pHeight*pSubDensity)
@@ -84,18 +120,25 @@ def generateTerrain():
     tempList = []
 
     vtx = getVtxId(0,0,(pWidth * pSubDensity)+1, (pHeight * pSubDensity)+1)
-    print('VTX: {}'.format(vtx))
     cmds.select('{}.vtx[{}]'.format(pTerrain[0], vtx))
 
     perms = createPermutation()
 
     # print('createPerm: {} len: {}'.format(perms, len(perms)))
+    count = 0
 
     # Go though each of the z values
     for z in range((pHeight * pSubDensity) + 1):
         for x in range((pWidth * pSubDensity) + 1):
-            n = Noise2D(x*0.01, z*0.01)
+            #print('x: {}'.format(x))
+            n = Noise2D(x*0.05, z*0.05, perms)
+            #print('n val: {}'.format(n))
             # As we are working in 2 dimensions, Noise2D will return values from -1.0f to 1.0f, we can map this to a range that we want later.
+            n = mapValue(0.0, 1.0, pMinHeight, pMaxHeight, n)
+            cmds.select('{}.vtx[{}]'.format(pTerrain[0], count))
+            cmds.move(0,n,0,r=True)
+            count+=1
+            
 
 
 
